ad5b3d33d821695f04101adf93ffc28d38b79475
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2023-01-23 23:36:57 +0100

    feat: added static props to product details page

--- src/pages/[pid].js
+++ src/pages/[pid].js
@@ -1,12 +1,30 @@
 import React from "react";
-import { useRouter } from "next/router";
+import fs from "fs/promises";
+import path from "path";
 
-function ProductInformation() {
-  const { query } = useRouter();
-  
-  const id = query.pid;
+function ProductInformation(props) {
+  const { product } = props;
+  return (
+    <>
+      <div>{product.title}</div>
+      <div>{product.description}</div>
+    </>
+  );
+}
 
-  return <div>{id}</div>;
+export async function getStatic(context) {
+  const { params } = context;
+  const {pid} = params
+
+  const filePath = path.join(process.cwd(), "src", "data", products.json);
+  const jsonData = fs.readFile(filePath);
+  const products = JSON.parse(jsonData);
+  const product = products.find((product)=> product.id === pid);
+  return {
+    props: {
+      product,
+    },
+  };
 }
 
 export default ProductInformation;
