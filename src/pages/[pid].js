c95e1f041dd35661a7c52e9fdb6aad64ab4ee7a0
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2023-01-24 00:49:00 +0100

    feat: dynamically rendering static paths

--- src/pages/[pid].js
+++ src/pages/[pid].js
@@ -17,12 +17,22 @@ function ProductInformation(props) {
   );
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "src", "data", "products.json");
+  const jsonData = await fs.readFile(filePath);
+  const products = JSON.parse(jsonData);
+
+  return products;
+}
+
 export async function getStaticPaths() {
+  const products = await getData();
+  const productsIds = products.map((product) => product.id);
+  const productIdParams = productsIds.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [
-      { params: { pid: "p1" } },
-    ],
-    fallback: true,
+    paths: productIdParams,
+    fallback: false,
   };
 }
 
@@ -30,9 +40,8 @@ export async function getStaticProps(context) {
   const { params } = context;
   const { pid } = params;
 
-  const filePath = path.join(process.cwd(), "src", "data", "products.json");
-  const jsonData = await fs.readFile(filePath);
-  const products = JSON.parse(jsonData);
+  const products = await getData();
+
   const product = products.find((product) => product.id === pid);
   return {
     props: {
