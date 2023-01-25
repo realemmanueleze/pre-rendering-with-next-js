6ed8806c74ccb625e073be5b3c2fa5b6e70119ce
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2023-01-25 23:51:52 +0100

    removed unused code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 import fs from "fs/promises";
 import path from "path";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home(props) {
   const { products } = props;
